4f08a374da34933d3f06cc0a99d64c0e356a26e7
maxfukuh4ra <maxfuku04@g.ucla.edu>
2024-10-15 14:58:31 -0700

    set-up for gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     modules: {
+//       localsConvention: "camelCase",
+//     }
+//   }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    }
+  base: '/portfolio/',  // Replace 'my-project' with your GitHub repo name
+  build: {
+    outDir: 'dist',
   }
-})
+});
