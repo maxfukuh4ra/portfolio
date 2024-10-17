bcac18eee40ca8f31088e42fbd031fde0da1cc38
maxfukuh4ra <maxfuku04@g.ucla.edu>
2024-10-17 15:21:37 -0700

    landing url fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route 
-              path="/" 
+              path="/portfolio" 
               element={
                 <>
                   <Hero />
