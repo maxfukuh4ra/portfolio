6b12927307fe35cd0c35663977882964543b35b7
maxfukuh4ra <maxfuku04@g.ucla.edu>
2024-10-17 13:18:20 -0700

    photos added

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,44 +3,48 @@ import React from "react";
 import styles from "./about.module.css";
 import { getImageUrl } from "../../utils";
 
+import { IoCameraSharp } from "react-icons/io5";
+import { PiSoccerBallFill } from "react-icons/pi";
+import { BsServer } from "react-icons/bs";
+
 export const About = () => {
   return (
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/aboutImage.png")}
+          src={getImageUrl("about/hero.png")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <BsServer size={150} style={{ marginRight: '20px' }}/>
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>Full-Stack Developer</h3>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
+                I'm a full-stack developer with experience in building responsive
+                and optimized sites both in the front and back-end. 
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <IoCameraSharp size={180} style={{ marginRight: '20px' }}/>
             <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
+              <h3>Designer & Producer</h3>
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+                I have experience creating professional quality flyers, video filming, video editing, 
+                and, merch / gear, utilizing Sony Alpha a7ii, gimbal, Adobe Illustrator, and Adobe Premiere Pro.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <PiSoccerBallFill size={150} style={{ marginRight: '20px' }}/>
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
+              <h3>Athlete</h3>
               <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+                As a retired soccer player, karate kid, tennis enthusiast, and current competitive dancer, 
+                teamwork is the success to everything.
               </p>
             </div>
           </li>
